9fb6eb4b8052bb076abdd9975d7a99434d705013
Erold Ramos <dev.eroldramos@gmail.com>
2022-11-22 09:54:13 +0800

    adding first github action

--- app/routers/user.py
+++ app/routers/user.py
@@ -14,10 +14,14 @@ def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
+    
+    # try:
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
+    # except:
+    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+    #                         detail = f"Email {new_user.email} already exists!")
     return new_user
 @router.get("/{id}", response_model=schemas.UserOut)
 def get_user(id:int,  db: Session = Depends(get_db)):
